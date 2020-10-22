9b564d411709a02adcab2e8fccad9325b1c0ef9a
Josh Cooper <joshcooper@users.noreply.github.com>
2020-10-22 16:34:13 -0700

    (PUP-10713) Pass expiration time to expired? method

--- lib/puppet/environments.rb
+++ lib/puppet/environments.rb
@@ -420,7 +420,7 @@ module Puppet::Environments
     def clear_all_expired()
       t = Time.now
       return if t < @next_expiration && ! @cache.any? {|name, _| @cache_expiration_service.expired?(name.to_sym) }
-      to_expire = @cache.select { |name, entry| entry.expires < t || @cache_expiration_service.expired?(name.to_sym) }
+      to_expire = @cache.select { |name, entry| entry.expired?(t) || @cache_expiration_service.expired?(name.to_sym) }
       to_expire.each do |name, entry|
         Puppet.debug {"Evicting cache entry for environment '#{name}'"}
         @cache_expiration_service.evicted(name.to_sym)
@@ -466,7 +466,8 @@ module Puppet::Environments
     # Evicts the entry if it has expired
     # Also clears caches in Settings that may prevent the entry from being updated
     def evict_if_expired(name)
-      if (result = @cache[name]) && (result.expired? || @cache_expiration_service.expired?(name.to_sym))
+      t = Time.now
+      if (entry = @cache[name]) && (entry.expired?(t) || @cache_expiration_service.expired?(name.to_sym))
         Puppet.debug {"Evicting cache entry for environment '#{name}'"}
         @cache_expiration_service.evicted(name.to_sym)
         clear(name)
@@ -485,7 +486,7 @@ module Puppet::Environments
       def touch
       end
 
-      def expired?
+      def expired?(now)
         false
       end
 
@@ -500,7 +501,7 @@ module Puppet::Environments
 
     # Always evicting entry
     class NotCachedEntry < Entry
-      def expired?
+      def expired?(now)
         true
       end
 
@@ -527,8 +528,8 @@ module Puppet::Environments
         @ttl = Time.now + @ttl_seconds
       end
 
-      def expired?
-        Time.now > @ttl
+      def expired?(now)
+        now > @ttl
       end
 
       def expires
